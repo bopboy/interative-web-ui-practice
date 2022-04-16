ce9b07ba5f948653bfa96c0dc4013275b20c3898
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-16 15:15:33 +0900

    이전, 다음 버튼 추가 및 기능 추가

--- music-player/js/main.js
+++ music-player/js/main.js
@@ -1,5 +1,9 @@
 const frame = document.querySelector("section")
 const lists = frame.querySelectorAll("article")
+const prev = document.querySelector(".btnPrev")
+const next = document.querySelector(".btnNext")
+const audio = frame.querySelectorAll("audio")
+
 const deg = 45
 const len = lists.length - 1
 let i = 0
@@ -15,12 +19,18 @@ for (let el of lists) {
     let load = el.querySelector(".load")
 
     play.addEventListener("click", e => {
-        e.currentTarget.closest("article").querySelector(".pic").classList.add("on")
-        e.currentTarget.closest("article").querySelector("audio").play()
+        let isActive = e.currentTarget.closest("article").classList.contains("on")
+        if (isActive) {
+            e.currentTarget.closest("article").querySelector(".pic").classList.add("on")
+            e.currentTarget.closest("article").querySelector("audio").play()
+        }
     })
     pause.addEventListener("click", e => {
-        e.currentTarget.closest("article").querySelector(".pic").classList.remove("on")
-        e.currentTarget.closest("article").querySelector("audio").pause()
+        let isActive = e.currentTarget.closest("article").classList.contains("on")
+        if (isActive) {
+            e.currentTarget.closest("article").querySelector(".pic").classList.remove("on")
+            e.currentTarget.closest("article").querySelector("audio").pause()
+        }
     })
     load.addEventListener("click", e => {
         e.currentTarget.closest("article").querySelector(".pic").classList.add("on")
@@ -29,3 +39,36 @@ for (let el of lists) {
     })
 
 }
+
+let active = 0
+let num = 0
+
+function activation(index, lists) {
+    for (let el of lists) {
+        el.classList.remove("on")
+    }
+    lists[index].classList.add("on")
+}
+
+function initMusic() {
+    for (let el of audio) {
+        el.pause()
+        el.load()
+        el.parentElement.previousElementSibling.classList.remove("on")
+    }
+}
+
+prev.addEventListener("click", () => {
+    initMusic()
+    num++;
+    frame.style.transform = `rotate(${deg * num}deg)`
+    active === 0 ? active = len : active--
+    activation(active, lists)
+})
+next.addEventListener("click", () => {
+    initMusic()
+    num--;
+    frame.style.transform = `rotate(${deg * num}deg)`
+    active === len ? active = 0 : active++
+    activation(active, lists)
+})
